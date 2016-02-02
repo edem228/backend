d10ca87035724ffc32ffe387a223a2854576c36e
Edem <edem228@gmail.com>
2016-02-02 01:53:20 +0100

    octoly json reader

---
+++ backend/answer.rb
@@ -0,0 +1,15 @@
+require "json"
+file =  File.read('videos.json')
+hash = JSON.parse(file)
+videos = []
+hash.each do |item|
+  videos << item
+end
+
+most_viewed = videos.map.sort_by do |i|
+   i["views_count"]
+end.last
+
+result = most_viewed["views_count"] * most_viewed["likes_count"] * most_viewed["dislikes_count"] * most_viewed["topic_ids"].length.to_i
+
+puts result.to_s + "@octoly.com"
